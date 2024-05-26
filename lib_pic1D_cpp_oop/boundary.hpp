56463e19da1a3bf86efd59fd1170ae413b27ce76
Keita-A <keita19990517@gmail.com>
2024-05-26 12:55:31 +0900

    20240526

---
+++ lib_pic1D_cpp_oop/boundary.hpp
@@ -0,0 +1,27 @@
+#include "const.hpp"
+#include "particle_struct.hpp"
+
+
+class Boundary
+{
+private:
+
+public:
+
+    void periodicBoundaryParticle();
+    void conductingWallBoundaryParticle();
+
+    void periodicBoundaryB();
+    void conductingWallBoundaryB();
+
+    void periodicBoundaryE();
+    void conductingWallBoundaryE();
+
+    void periodicBoundaryCurrent();
+    void conductingWallBoundaryCurrent();
+
+private:
+
+};
+
+
